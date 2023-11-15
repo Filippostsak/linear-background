0a19f05d0fc77c4eceb14333891dc968fe2ddd09
Filippostsak <filippostsak@gmail.com>
2023-11-15 12:01:57 +0200

    update

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var randomButton = document.getElementById("randomButton");
-var button = document.querySelector("button");
-var input = document.querySelectorAll("input")
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let randomButton = document.getElementById("randomButton");
+let button = document.querySelector("button");
+let input = document.querySelectorAll("input");
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 randomButton.addEventListener("click", generateRandomColors);
 
-generateRandomColors(); 
+generateRandomColors();
 
 function setGradient() {
-  body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
   css.textContent = body.style.background + ";";
-  button.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  button.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 }
 
 function generateRandomColors() {
-  var randomColor1 = getRandomColor();
-  var randomColor2 = getRandomColor();
+  let randomColor1 = getRandomColor();
+  let randomColor2 = getRandomColor();
 
   color1.value = randomColor1;
   color2.value = randomColor2;
@@ -29,14 +31,10 @@ function generateRandomColors() {
 }
 
 function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
+  let letters = "0123456789ABCDEF";
+  let color = "#";
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
-
-
-
-
